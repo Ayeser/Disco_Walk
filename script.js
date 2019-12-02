4e83243d21ceaa85eb4e8be1e023eb536153aa19
Matthew Wester <MatthewWester@live.com>
2019-12-01 21:22:51 -0700

    User can now tap the tempo for the song to play

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function play(){
+let lastTap = 0;
+
+function play() {
     var audio = document.getElementById("audio");
     audio.loop = true;
     var rate = (document.getElementById("tempo").value / 103);
     audio.playbackRate = rate;
     audio.play();
-              }
+}
+
+function tap() {
+    var d = new Date();
+    var t = d.getTime();
+    if(t - lastTap < 3000) {
+        let tempo = ((1000/(t-lastTap))*60);
+        document.getElementById("tappedTempo").innerHTML = "The tapped tempo is now " + tempo;
+        audio.play();
+    }
+    lastTap = t;
+}
